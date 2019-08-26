19a92f89a0a8762e8371f4736d99653b1a1a5962
imranqau5373 <imranqau5373@gmail.com>
2019-08-26 17:19:49 +0500

    new commit

---
+++ precache-manifest.df726b73fa9ae97aa994adb171cf4d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2686bc0ee6ff48ee75fb6cdf0fc660dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7851f70414839cff666",
+    "url": "/static/css/2.7d2d46fe.chunk.css"
+  },
+  {
+    "revision": "5b52d165fda54495b1da",
+    "url": "/static/css/main.c3b4e87a.chunk.css"
+  },
+  {
+    "revision": "e7851f70414839cff666",
+    "url": "/static/js/2.9d510eae.chunk.js"
+  },
+  {
+    "revision": "5b52d165fda54495b1da",
+    "url": "/static/js/main.673ec075.chunk.js"
+  },
+  {
+    "revision": "f2e503d609549037b5e6",
+    "url": "/static/js/runtime~main.7c870d60.js"
+  },
+  {
+    "revision": "5cac23da84a26939cdd436028a71451a",
+    "url": "/static/media/logo.5cac23da.svg"
+  }
+]);
